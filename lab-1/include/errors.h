39a0029a07dabd11a7a886626ea4b9b3dc94edf6
iannsgirdye <ivan4ik.babi4ik777@yandex.ru>
2025-10-02 16:15:47 +0300

    lab-1: Add functions for errors in parent

---
+++ lab-1/include/errors.h
@@ -0,0 +1,10 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+
+void errorPipe();
+void errorFork();
+void errorExecv();
+
+
+#endif
---
+++ lab-1/src/errors.c
@@ -0,0 +1,25 @@
+#include "../include/errors.h"
+#include "../include/colors.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void errorPipe() {
+  const char *message = COLOR_BOLD_RED "error: " COLOR_WHITE "pipe failed\n";
+  write(STDERR_FILENO, message, sizeof(message));
+  exit(EXIT_FAILURE);
+}
+
+
+void errorFork() {
+  const char *message = COLOR_BOLD_RED "error: " COLOR_WHITE "fork failed\n";
+  write(STDERR_FILENO, message, sizeof(message));
+  exit(EXIT_FAILURE);
+}
+
+
+void errorExecv() {
+  const char *message = COLOR_BOLD_RED "error: " COLOR_WHITE "exec child\n";
+  write(STDERR_FILENO, message, sizeof(message));
+  exit(EXIT_FAILURE);
+}
