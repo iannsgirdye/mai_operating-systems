17a8bc75c9b5d900044a72413556b0645f4c5250
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-23 14:47:13 +0300

    lab-4: Add function "pi" to "lib1"

--- lab-4/src/lib1.c
+++ lab-4/src/lib1.c
@@ -26,3 +26,11 @@ int prime_count(int a, int b) {
   }
   return count;
 }
+
+float pi(int k) {
+  float sum = 0.0;
+  for (int i = 0; i != k; ++i) {
+    sum += (i % 2 == 0 ? 1.0 : -1.0) / (2.0 * i + 1.0);
+  }
+  return sum * 4.0;
+}
