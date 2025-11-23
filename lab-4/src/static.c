efa61ee89a5aec73e0252f56021bd58832b6579c
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-23 18:01:44 +0300

    lab-4: Add static program

---
+++ lab-4/src/static.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "../include/contracts.h"
+#include "../include/utilities.h"
+
+#define BUFFER_SIZE 128
+
+int main() {
+  printMessage("1 <start> <end> - count of prime numbers in range [start; end]\n2 <accuracy> - PI\n\n");
+
+  char buffer[BUFFER_SIZE];
+  int mode = 0;
+  int arg1 = 0;
+  int arg2 = 0;
+  ssize_t len;
+  int argsCount;
+
+  while ((len = read(STDIN_FILENO, buffer, BUFFER_SIZE - 1)) > 0) {
+    buffer[len] = '\0';
+    argsCount = sscanf(buffer, "%d%d%d", &mode, &arg1, &arg2);
+    
+    switch(checkCommand(mode, argsCount)) {
+      case NO_COMMAND:
+        printError("Invalid input\n");
+        break;
+      case INVALID_MODE:
+        printError("Invalid mode\n");
+        break;
+      case INVALID_ARGS_COUNT:
+        printError("Invalid count of arguments\n");
+        break;
+      default:
+        switch (mode) {
+          case 1:
+            snprintf(buffer, BUFFER_SIZE, "%d\n", prime_count(arg1, arg2));
+            write(STDOUT_FILENO, buffer, strlen(buffer));
+            break;
+          case 2:
+            snprintf(buffer, BUFFER_SIZE, "%f\n", pi(arg1));
+            write(STDOUT_FILENO, buffer, strlen(buffer));
+            break;
+        }
+    }
+  }
+
+  return 0;
+}
